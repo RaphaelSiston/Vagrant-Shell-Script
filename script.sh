269502920ac36da4aa16fbe423b620377faf92e9
Raphael Siston <132157444+RaphaelSiston@users.noreply.github.com>
2023-06-15 22:36:03 -0300

    Delete script.sh

--- script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-Instalação de pacotes = vim curl telnet unzip wget net-tools htop nmap 
-
-apt update -y
-apt install -y 
-
-hostnamectl set-hostname Projeto01
-sudo adduser "raphaelsiston"
